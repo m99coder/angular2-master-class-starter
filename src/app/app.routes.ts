2804f80768bf71f226e6a24184a84ef8ae69b0f8
Marco Lehmann <marco.lehmann@kiwigrid.com>
2016-10-26 14:33:27 +0200

    added type information to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
-import { ContactsListComponent } from './contacts-list/contacts-list.component';
-import { ContactsDetailComponent } from './contacts-detail/contacts-detail.component';
+import { Routes } from '@angular/router';
 
-export const ContactsAppRoutes = [
+import { ContactsDetailComponent } from './contacts-detail/contacts-detail.component';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+
+export const ContactsAppRoutes: Routes = [
   {path: '', component: ContactsListComponent},
   {path: 'contact/:id', component: ContactsDetailComponent}
 ];
