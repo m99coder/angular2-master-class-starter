84aac62256c26800f5d97906058a61ef8e2daa27
Marco Lehmann <marco.lehmann@kiwigrid.com>
2016-10-26 14:09:01 +0200

    module-1-step-5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { ContactsAppComponent } from './contacts.component';
 import { ContactsHeaderComponent } from './contacts-header/contacts-header.component';
 import { ContactsService } from './contacts.service';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { ContactsAppRoutes } from './app.routes';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [ContactsAppComponent, ContactsHeaderComponent],
-  imports: [BrowserModule],
+  declarations: [ContactsAppComponent, ContactsHeaderComponent, ContactsListComponent],
+  imports: [BrowserModule, RouterModule.forRoot(ContactsAppRoutes)],
   bootstrap: [ContactsAppComponent],
   providers: [ContactsService]
 })
