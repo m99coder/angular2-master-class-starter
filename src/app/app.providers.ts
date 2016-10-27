a5f84e550f7257332e6121fc79696031b4e34e82
Marco Lehmann <marco.lehmann@kiwigrid.com>
2016-10-27 15:38:53 +0200

    added title service usage

--- src/app/app.providers.ts
+++ src/app/app.providers.ts
@@ -1,3 +1,5 @@
+import { Title } from '@angular/platform-browser';
+
 import { ContactsService } from './contacts.service';
 import { API_ENDPOINT } from './app.tokens';
 import { EventBusService } from './event-bus.service';
@@ -5,5 +7,6 @@ import { EventBusService } from './event-bus.service';
 export const APP_PROVIDERS = [
   {provide: API_ENDPOINT, useValue: 'http://localhost:4201/api'},
   ContactsService,
-  EventBusService
+  EventBusService,
+  Title
 ];
--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { EventBusService, APP_TITLE_CHANGE } from './event-bus.service';
 
 @Component({
@@ -10,14 +11,20 @@ export class ContactsAppComponent implements OnInit {
 
   title: string;
 
-  constructor(private eventBusService: EventBusService) {
+  constructor(
+    private eventBusService: EventBusService,
+    private titleService: Title
+  ) {
 
   }
 
   ngOnInit() {
     this.eventBusService
       .observe(APP_TITLE_CHANGE)
-      .subscribe(title => this.title = title);
+      .subscribe(title => {
+        this.title = title;
+        this.titleService.setTitle(title);
+      });
   }
 
 }
